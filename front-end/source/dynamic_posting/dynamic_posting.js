f9161e03b54c9f50e257fdf7da5d33e166be3494
Hero <jacobtaylor048@gmail.com>
2025-04-07 12:46:35 -0400

    Added remove post functionality

--- front-end/source/dynamic_posting/dynamic_posting.js
+++ front-end/source/dynamic_posting/dynamic_posting.js
@@ -1,9 +1,10 @@
+const { error } = require("console");
 const fs = require("fs");
 
 const FILE_PATH = "../Fake-Server/server.json";
 
 /**
- * this function reads from the fake_server.json and returns the json representation of the contents.
+ * this function reads from  server.json and returns the json representation of the contents.
  *
  * @returns {JSON}
  */
@@ -20,13 +21,44 @@ function fetch_server_data() {
 /**
  * Given a json representation of a post, this function appends the new post data to server.json
  *
- * @param {JSON} input_post_data post data formatted in JSON
+ * @param {JSON} new_post post data formatted in JSON
  * @returns {void}
  */
-function append_server_data(input_post_data) {
+function append_new_post(new_post) {
   const jsonData = transverse_post_data();
 
-  jsonData.newSection = input_post_data;
+  if (Array.isArray(jsonData.posts)) {
+    jsonData.posts.push(newPost);
+  } else {
+    // If posts doesn't exist or isn't an array, initialize it
+    jsonData.posts = [newPost];
+  }
+
+  const updatedJson = JSON.stringify(jsonData, null, 2);
+
+  try {
+    fs.writeFileSync(FILE_PATH, updatedJson, "utf-8");
+  } catch (error) {
+    console.log(`Error writing to file:${error}`);
+  }
+}
+
+/**
+ * Given a json representation of a post, this function deletes the post from  server.json
+ *
+ * @param {JSON} new_post post data formatted in JSON
+ * @returns {void}
+ */
+function remove_post(post) {
+  const jsonData = transverse_post_data();
+
+  let index = jsonData.posts.indexOf(post);
+
+  if (Array.isArray(jsonData.posts) && index !== -1) {
+    jsonData.posts.splice(index, 1);
+  } else {
+    console.log("Posts array does not exist");
+  }
 
   const updatedJson = JSON.stringify(jsonData, null, 2);
 
@@ -43,6 +75,4 @@ function append_server_data(input_post_data) {
  */
 function generate_posts() {
   const json_data = fetch_server_data();
-  
-
 }
