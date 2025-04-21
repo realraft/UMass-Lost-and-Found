e50d267cc918076fd0bf603d9a4a36ee265a51c3
Robrufe <rrubiofernan@umass.edu>
2025-04-20 22:35:55 -0400

    Add user model, routes, and server setup

---
+++ back-end/source/Server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import taskRoutes from "../routes/taskRoutes";
+import userRoutes from "../routes/userRoutes";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
